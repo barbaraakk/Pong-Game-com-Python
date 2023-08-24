0ea2d12a9cab1230fc087b6ad489ec2d6b21abdd
Bárbara Müller <105740844+barbaraakk@users.noreply.github.com>
2023-08-24 10:49:16 -0300

    Update

---
+++ Pong-Game-com-Python-main/backend/testes/testar.py
@@ -0,0 +1,12 @@
+from backend.geral.config import *
+from backend.modelo.jogador import Jogador
+
+
+def run():
+    print("TESTE DE JOGADOR")
+    
+    j1 = Jogador(pontos = 0)
+    db.session.add(j1)
+    db.session.commit()
+    print(j1)
+    print(j1.json())
